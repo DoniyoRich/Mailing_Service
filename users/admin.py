af3f9b3c2f88accc8385639ca061163e1051d21d
Doniyor <donish1979@gmail.com>
2025-05-15 23:46:33 +0300

    initial commit for feat./mailing-service branch. Migrated model CustomUser, created my_tags filter

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "phone_number", "country")
+    list_filter = ("email",)
+    search_fields = ("email",)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=150, unique=True, blank=True, null=True)
+    email = models.EmailField(unique=True, verbose_name='e-mail')
+    avatar = models.ImageField(upload_to='users/avatars/', verbose_name='Аватар', blank=True, null=True)
+    phone_number = models.CharField(max_length=15, verbose_name='Номер телефона', blank=True, null=True)
+    country = models.CharField(max_length=50, verbose_name='Страна', blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username',]
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
+
